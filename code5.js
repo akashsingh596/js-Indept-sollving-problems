4d89ca6dc6ce6072222c0560235c4b27205440b5
akashsingh596 <72357922+akashsingh596@users.noreply.github.com>
2025-02-25 15:39:44 +0530

    swapping

--- code5.js
+++ code5.js
@@ -1,1 +1,31 @@
-// write a code 
+// swaping variable by three methods
+
+// 1 - swap with extra variable 
+
+var a= 10;
+var b= 20;
+var c = a 
+a=b
+b=c
+// console.log(c)
+// console.log(a)
+// console.log(b)
+
+// swap without using extra variable 
+var a= 10;
+var b= 20;
+
+a = a+b
+// console.log(a)
+b = a-b
+// console.log(b)
+a = a-b
+// console.log(a)
+
+// swap using array method (array destructuring)
+
+var a = 10;
+var b= 20;
+[a,b] = [b,a]
+// console.log(a)
+// console.log(b)
